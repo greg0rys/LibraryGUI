81650bb9606eb180e76cb4931d0ed93353b51b67
greg0rys <gshenefel@greg0rys.codes>
2024-07-04 15:44:45 -0700

    2

--- src/Presentation/BookMenu.java
+++ src/Presentation/BookMenu.java
@@ -1,8 +1,13 @@
 package Presentation;
 
-import javax.swing.*;
+import Logic.BookManager;
 
-public class BookMenu
+import javax.swing.*;
+import java.awt.*;
+import java.awt.print.Book;
+import java.util.ArrayList;
+
+public class BookMenu extends JFrame
 {
     private JPanel root;
     private JButton homeButton;
@@ -10,4 +15,35 @@ public class BookMenu
     private JButton findBookButton;
     private JButton addBookButton;
     private JButton seeBookLoanStatusButton;
+    private  BookManager bookManager;
+    private ArrayList<Book>
+
+    public BookMenu()
+    {
+        super("Book Menu");
+        load();
+        bookManager = new BookManager();
+        add(root);
+        setPreferredSize(new Dimension(400, 200));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        pack();
+    }
+
+    /**
+     * Load the book manager.
+     */
+    private void load()
+    {
+
+    }
+
+    /**
+     * When new bookMenus get created pass the current manager.
+     * @return
+     */
+    public  BookManager getBookManager()
+    {
+        return bookManager;
+    }
 }
